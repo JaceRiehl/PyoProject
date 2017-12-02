ba2e986a4df2f107783487dabfc21a54a7d42334
Riehl <jace.riehl@w518-13.local>
2017-12-02 15:15:55 -0700

    Added code to turn the midi converter into a pyo synthesizer

--- seq_demo.py
+++ seq_demo.py
@@ -5,9 +5,12 @@ freq_x = midiToHz(chord_x)
 
 s = Server().boot()
 
+
+
 env = CosTable([(0,0),(300,1),(1000,.3),(8191,0)])
 seq = Seq(time=.2, seq=[2,1,2,2,2,1,2,2,2,2,1,2,1,2,2,2,1,2,2,1,2], poly=1).play()
 amp = TrigEnv(seq, table=env, dur=.25, mul=.2)
+#have to change freq_x based on the chord 
 a = SineLoop(freq=freq_x, feedback=0.09, mul=amp).out()
 
 s.gui(locals())
